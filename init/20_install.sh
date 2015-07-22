8c817c858a04a00652930989da436bbc46c258f9
Sean Dion <smdion@gmail.com>
2015-07-22 13:21:48 -0500

    change user in apache variables

--- init/20_install.sh
+++ init/20_install.sh
@@ -17,8 +17,8 @@ cp /defaults/apache2.conf /etc/apache2/apache2.conf
 cp /defaults/ports.conf /etc/apache2/ports.conf
 
 # Manually set the apache environment variables in order to get apache to work immediately.
-echo www-data > /etc/container_environment/APACHE_RUN_USER
-echo www-data > /etc/container_environment/APACHE_RUN_GROUP
+echo abc > /etc/container_environment/APACHE_RUN_USER
+echo abc > /etc/container_environment/APACHE_RUN_GROUP
 echo /var/log/apache2 > /etc/container_environment/APACHE_LOG_DIR
 echo /var/lock/apache2 > /etc/container_environment/APACHE_LOCK_DIR
 echo /var/run/apache2.pid > /etc/container_environment/APACHE_PID_FILE
