4cf294294f29ceb284ed3709ec495a0d2107018a
Sean Dion <smdion@gmail.com>
2015-07-23 16:01:03 -0500

    Update 20_check_config.sh

--- init/20_check_config.sh
+++ init/20_check_config.sh
@@ -2,6 +2,8 @@
 
 # Check if config exists. If not, copy in the sample config
 if [ -f /etc/apache2/sites-available/000-default.conf ]; then
+  chown -R abc:abc /etc/apache2/sites-available/
+  service apache2 restart
   echo "Using existing config file."
 else
   echo "Creating config from template."
