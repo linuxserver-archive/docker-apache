bca71956f0e1524c8ae2f3eb1f45af1ede19e9b3
Sean Dion <smdion@gmail.com>
2015-07-22 20:59:42 -0500

    fix permissions on /web

--- init/10_new_user.sh
+++ init/10_new_user.sh
@@ -3,6 +3,8 @@
 if [ ! "$(id -u abc)" -eq "$PUID" ]; then usermod -o -u "$PUID" abc ; fi
 if [ ! "$(id -g abc)" -eq "$PGID" ]; then groupmod -o -g "$PGID" abc ; fi
 chown abc:abc -R /config
+chown abc:abc -R /web
+
 echo "
 -----------------------------------
 GID/UID
