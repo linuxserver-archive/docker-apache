9aaa7f0b00d115de523adc1442bde13bc29fd245
Sean Dion <smdion@gmail.com>
2015-08-02 17:37:21 -0500

    Update 10_new_user.sh

--- init/10_new_user.sh
+++ init/10_new_user.sh
@@ -2,9 +2,9 @@
 
 if [ ! "$(id -u abc)" -eq "$PUID" ]; then usermod -o -u "$PUID" abc ; fi
 if [ ! "$(id -g abc)" -eq "$PGID" ]; then groupmod -o -g "$PGID" abc ; fi
-chown -R abc:abc /config/
-chown -R abc:abc /etc/apache2/
-chown -R abc:abc /var/log/apache2/
+chown -R abc:abc /config
+chown -R abc:abc /www
+chown -R abc:abc /logs
 
 echo "
 -----------------------------------
