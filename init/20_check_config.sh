9253d5606c696dd97b57be61b036fb547147cd3a
Sean Dion <smdion@gmail.com>
2015-08-02 17:38:54 -0500

    Update 20_check_config.sh

--- init/20_check_config.sh
+++ init/20_check_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Check if config exists. If not, copy in the sample config
-if [ -f /config/apache2/000-default.conf ]; then
+if [ -f /config/apache2/proxy-config.conf ]; then
   echo "Using existing config file."
 else
   echo "Creating config from template."
-  mv /defaults/000-default.conf /config/apache2/000-default.conf
+  mv /defaults/000-default.conf /config/proxy-config.conf
   chown abc:abc /config/apache2/sites-available/000-default.conf
 fi
