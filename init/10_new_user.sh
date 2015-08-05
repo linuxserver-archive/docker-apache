0b2d896e99b5763b34df5dc31bae6fb2b33d814b
Sean Dion <smdion@gmail.com>
2015-08-04 19:27:25 -0500

    remove non-existant directories

--- init/10_new_user.sh
+++ init/10_new_user.sh
@@ -3,8 +3,6 @@
 if [ ! "$(id -u abc)" -eq "$PUID" ]; then usermod -o -u "$PUID" abc ; fi
 if [ ! "$(id -g abc)" -eq "$PGID" ]; then groupmod -o -g "$PGID" abc ; fi
 chown -R abc:abc /config
-chown -R abc:abc /web
-chown -R abc:abc /logs
 
 echo "
 -----------------------------------
